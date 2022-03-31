a2becc816944a1ded0313e8de1cb434bc13cac3b
Your Name <elik@example.com>
2022-03-31 12:08:35 +0300

    Добавьте файлы проекта.

---
+++ Laba_3_kry/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_3_kry
+{
+    public partial class Form1 : Form
+    {
+        public class Freq
+        {
+            char n;
+            int f;
+            
+            public Freq(char nn)
+            {
+                this.n = nn;
+               // this.f = nf;
+            }
+        }
+
+
+      
+        
+        public Form1()
+        {
+            string str = richTextBox1.Text;
+            List<Freq> frq = new List<Freq>();
+            foreach (char i in str)
+            {
+                frq.Add(i);
+            }
+            InitializeComponent();
+        }
+    }
+}
---
+++ Laba_3_kry/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Laba_3_kry")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Laba_3_kry")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("03c75136-bb6f-425a-b63c-dc7ec1a14079")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

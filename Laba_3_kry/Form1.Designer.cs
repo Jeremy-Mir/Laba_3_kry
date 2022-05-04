71322acdb41e3d3306d35836ff89657160ad6b03
Еремей Миронов <yerem01@mail.ru>
2022-05-04 14:16:46 +0300

    123

--- Laba_3_kry/Form1.Designer.cs
+++ Laba_3_kry/Form1.Designer.cs
@@ -32,6 +32,9 @@ namespace Laba_3_kry
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -60,11 +63,42 @@ namespace Laba_3_kry
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(13, 327);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(775, 168);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(350, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(811, 667);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
@@ -79,6 +113,9 @@ namespace Laba_3_kry
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Laba_3_kry/Form1.cs
+++ Laba_3_kry/Form1.cs
@@ -12,14 +12,14 @@ namespace Laba_3_kry
 {
     public partial class Form1 : Form
     {
-       
 
 
-      
-        
+
+
+
         public Form1()
         {
-           
+
             InitializeComponent();
         }
 
@@ -27,6 +27,9 @@ namespace Laba_3_kry
         {
             public string I;
             public int N;
+            public string Code;
+            public Freq L = null;
+            public Freq R = null;
 
             public Freq(string nn, int num)
             {
@@ -35,10 +38,13 @@ namespace Laba_3_kry
             }
 
         }
-        private void button1_Click(object sender, EventArgs e)
+        List<Freq> frq = new List<Freq>();
+        List<Freq> frq_fin = new List<Freq>();
+        public Freq n = new Freq("", 0);
+        public void button1_Click(object sender, EventArgs e)
         {
             string str = richTextBox1.Text;
-            List<Freq> frq = new List<Freq>();
+            
 
             bool search = false;
             foreach (char i in str)
@@ -54,7 +60,7 @@ namespace Laba_3_kry
                 }
                 if (search == false)
                 {
-                    frq.Add(new Freq(i.ToString(), 0));
+                    frq.Add(new Freq(i.ToString(), 1));
                 }
                 search = false;
             }
@@ -64,6 +70,142 @@ namespace Laba_3_kry
                 Console.WriteLine(f.I + " = " + f.N);
             }
 
+            
+            int count = frq[0].N;
+            int number = 0;
+
+
+            while(frq.Count != 1) { 
+            n = new Freq("", 0);
+            number = 0;
+            count = frq[0].N;
+            foreach (Freq f in frq)
+            {
+                
+               if (f.N <= count)
+                {
+                    count = f.N;
+                    n.L = f;
+                    number = frq.IndexOf(f);
+                }
+            }
+            Console.WriteLine("Remuve = " + frq[number].I);
+            frq.Remove(frq[number]);
+            n.I = n.I + n.L.I;
+            n.N = n.N + n.L.N;
+
+            Console.WriteLine( "L = " + n.L.I);
+
+            
+
+            count = frq[0].N;
+            number = 0;
+            foreach (Freq f in frq)
+            {
+
+                if (f.N <= count)
+                {
+                    count = f.N;
+                    n.R = f;
+                    number = frq.IndexOf(f);
+                }
+            }
+            Console.WriteLine("Remuve = " + frq[number].I);
+
+            frq.Remove(frq[number]);
+            n.I = n.I + n.R.I;
+            n.N = n.N + n.R.N;
+            Console.WriteLine("R = " + n.R.I);
+            frq.Add(n);
+            foreach (Freq f in frq)
+            {
+
+                Console.WriteLine(f.I + " = " + f.N);
+            }
+            }
+            Console.WriteLine("####################################");
+           
+            foreach(char f in str)
+            {
+                Freq p = n;
+                string p_count = "";
+                while (p.I != f.ToString())
+                {
+                    bool b = true;
+                    foreach(char f_frq in p.L.I)
+                    {
+                        if (f == f_frq)
+                        {
+                            p_count = p_count + "1";
+                            p = p.L;
+                            b = false;
+                        }
+                    }
+                    if (b)
+                    {
+                        p_count = p_count + "0";
+                        p = p.R;
+                    }
+                }
+                p.Code = p_count;
+                Console.WriteLine("I = "+p.I + " count = "+ p.Code);
+                richTextBox2.Text = richTextBox2.Text + p.Code;
+                
+                
+            }
+            
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string str = richTextBox1.Text;
+           
+            foreach (char i in str)
+            {
+                i.ToString();
+                Console.WriteLine((byte)i);
+                string BinaryCode = Convert.ToString(i, 2);
+
+                richTextBox3.Text = richTextBox3.Text + BinaryCode;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string str = richTextBox2.Text;
+            Freq p = n;
+            foreach (char i in str)
+            {
+
+                if (p.I.Length == 1)
+                {
+                    richTextBox1.Text = richTextBox1.Text + p.I;
+                    p = n;
+                }
+                    if (i == '1')
+                    {
+                        p = p.L;
+                    
+                    }
+                    if (i == '0')
+                    {
+                        p = p.R;
+                    
+                }
+                Console.WriteLine(" i = " + p.I);
+            }
+            richTextBox1.Text = richTextBox1.Text + p.I;
+            p = n;
+
+
+
+
+        }
+
+
         }
     }
-}
+
